8539984d1c1763cd38ced9a070d0a4b34e2e8f32
kaminyou ho <kaminyouho@aetherai.com>
2022-01-31 22:28:41 +0800

    weight init & buffer

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,5 @@
+import random
+
 import albumentations as A
 import numpy as np
 import torch
@@ -67,3 +69,33 @@ def get_kernel(padding=1, gaussian_std=3, mode="constant"):
         raise NotImplementedError
     
     return kernel
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)
+
+class ReplayBuffer:
+    def __init__(self, max_size=50):
+        assert (max_size > 0), "Empty buffer or trying to create a black hole. Be careful."
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return torch.cat(to_return)
