8478327afb4585766778cf24ad7cab875f7d2f94
Min-Sheng <mason840929@gmail.com>
2022-02-09 03:35:22 +0000

    Allow multiple train_x and train_y

--- crop_pipeline.py
+++ crop_pipeline.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     config = read_yaml_config(args.config)['CROPPING_SETTING']
-    os.system(f"python3 crop.py -i {config['TRAIN_X']} -o {config['TRAIN_DIR_X']} --patch_size {config['PATCH_SIZE']} --stride {config['STRIDE']}")
-    os.system(f"python3 crop.py -i {config['TRAIN_Y']} -o {config['TRAIN_DIR_Y']} --patch_size {config['PATCH_SIZE']} --stride {config['STRIDE']}")
-    os.system(f"python3 crop.py -i {config['TEST_X']} -o {config['TEST_DIR_X']} --thumbnail --thumbnail_output {config['TEST_DIR_X']} --patch_size {config['PATCH_SIZE']} --stride {config['PATCH_SIZE']}")
+    for train_x, train_y in zip(config['TRAIN_X'], config['TRAIN_Y']):
+        os.system(f"python3 crop.py -i {train_x} -o {config['TRAIN_DIR_X']} --patch_size {config['PATCH_SIZE']} --stride {config['STRIDE']}")
+        os.system(f"python3 crop.py -i {train_y} -o {config['TRAIN_DIR_Y']} --patch_size {config['PATCH_SIZE']} --stride {config['STRIDE']}")
+    os.system(f"python3 crop.py -i {config['TEST_X']} -o {config['TEST_DIR_X']} --thumbnail_output {config['TEST_DIR_X']} --patch_size {config['PATCH_SIZE']} --stride {config['PATCH_SIZE']}")
