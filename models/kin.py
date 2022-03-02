88bf6ad39c2d3b2ce6202ccb9d53266f865d80da
kaminyou ho <kaminyouho@aetherai.com>
2022-03-02 10:38:30 +0800

    pending

--- models/kin.py
+++ models/kin.py
@@ -1,3 +1,4 @@
+# fomatiing!
 import itertools
 
 import numpy as np
@@ -32,69 +33,84 @@ class KernelizedInstanceNorm(nn.Module):
         self.std_table = torch.zeros(y_anchor_num, x_anchor_num, self.out_channels).to(self.device)
 
     def init_kernel(self, kernel=(torch.ones(1,1,3,3)/9)):
+        # modify 
         kernel = kernel.to(self.device)
         self.kernel = kernel
 
-    def collection(self, instance_means, instnace_stds, y_anchors, x_anchors):
+    def update_cache_table(self, instance_means, instance_stds, y_anchors, x_anchors):
         instance_means = instance_means.squeeze(-1).squeeze(-1)
-        instnace_stds = instnace_stds.squeeze(-1).squeeze(-1)
-        for instance_mean, instnace_std, y_anchor, x_anchor in zip(instance_means, instnace_stds, y_anchors, x_anchors):
+        instance_stds = instance_stds.squeeze(-1).squeeze(-1)
+        for instance_mean, instance_std, y_anchor, x_anchor in zip(
+            instance_means, instance_stds, y_anchors, x_anchors,
+        ):
             self.mean_table[y_anchor, x_anchor, :] = instance_mean
-            self.std_table[y_anchor, x_anchor, :] = instnace_std
+            self.std_table[y_anchor, x_anchor, :] = instance_std
     
-    def query_neighbors(self, y_anchor, x_anchor, padding=1):
-        """
-        return_anchors:: [top, down], [left, right] all are inclusive
-        """
-        y_anchor_top = max(0, y_anchor - padding)
-        y_anchor_down = min(self.y_anchor_num, y_anchor + padding)
-        x_anchor_left = max(0, x_anchor - padding)
-        x_anchor_right = min(self.x_anchor_num, x_anchor + padding)
-        return [y_anchor_top, y_anchor_down, x_anchor_left, x_anchor_right]
+    #def query_neighbors(self, y_anchor, x_anchor, padding=1):
+    #    """
+    #    return_anchors:: [top, down], [left, right] all are inclusive
+    #    """
+    #    y_anchor_top = max(0, y_anchor - padding)
+    #    y_anchor_down = min(self.y_anchor_num, y_anchor + padding)
+    #    x_anchor_left = max(0, x_anchor - padding)
+    #    x_anchor_right = min(self.x_anchor_num, x_anchor + padding)
+    #    return [y_anchor_top, y_anchor_down, x_anchor_left, x_anchor_right]
 
     def pad_table(self, padding):
+        # modify
+        # padded table shape inconsisency
         pad_func = nn.ReplicationPad2d((padding, padding, padding, padding))
-        self.padded_mean_table = pad_func(self.mean_table.permute(2, 0, 1).unsqueeze(0)) # [H, W, C] -> [C, H, W] -> [N, C, H, W]
-        self.padded_std_table = pad_func(self.std_table.permute(2, 0, 1).unsqueeze(0)) # [H, W, C] -> [C, H, W] -> [N, C, H, W]
+        self.padded_mean_table = pad_func(
+            self.mean_table.permute(2, 0, 1).unsqueeze(0)
+        ) # [H, W, C] -> [C, H, W] -> [N, C, H, W]
+        self.padded_std_table = pad_func(
+            self.std_table.permute(2, 0, 1).unsqueeze(0)
+        ) # [H, W, C] -> [C, H, W] -> [N, C, H, W]
     
     def __multiply_kernel(self, x_stat):
         # self.kernel = [1,1,H,W] || x_stat = [1,C,H,W]
+        # modify: check boardcasting of kernel [H, W]
+        # modify: use pytorch conv. F.conv
         assert self.kernel.shape[2:] == x_stat.shape[2:]
         x_stat = x_stat * self.kernel # [1,C,H,W] = [1,C,H,W] * [1,1,H,W]
         x_stat = x_stat.flatten(start_dim=2).sum(dim=2) # [1, C, H, W] -> [1, C, H * W] -> [1, C]
-        x_stat = x_stat.unsqueeze(-1).unsqueeze(-1) # [1, C] -> [1, C, 1, 1]
+        x_stat = x_stat.view(1, -1, 1, 1) # [1, C] -> [1, C, 1, 1]
         return x_stat
 
-    def forward_normal(self, x):
+    def forward_normal(self, x, eps=1e-9):
         x_mean, x_std = self.calc_mean_std(x)
-        x = (x - x_mean) / x_std #* self.weight + self.bias
+        x = (x - x_mean) / (x_std + eps) #* self.weight + self.bias
         return x
 
     def forward(self, x, y_anchor=None, x_anchor=None, padding=1):
         if self.training or self.normal_instance_normalization:
             return self.forward_normal(x)
 
+        x_mean, x_std = self.calc_mean_std(x)
+        assert y_anchor is not None
+        assert x_anchor is not None
+
+        if self.collection_mode:
+            self.update_cache_table(
+                instance_means=x_mean, 
+                instance_stds=x_std, 
+                y_anchors=y_anchor, 
+                x_anchors=x_anchor
+            )
+
         else:
-            x_mean, x_std = self.calc_mean_std(x)
-            assert y_anchor != None
-            assert x_anchor != None
+            assert x.shape[0] == 1 # currently, could support batch size = 1 for kernelized instance normalization
+            top = y_anchor
+            down = y_anchor + 2 * padding + 1
+            left = x_anchor
+            right = x_anchor + 2 *padding + 1
+            x_mean = self.padded_mean_table[:, :, top:down, left:right] # 1, C, H, W
+            x_std = self.padded_std_table[:, :, top:down, left:right] # 1, C, H, W
+            x_mean = self.__multiply_kernel(x_mean)
+            x_std = self.__multiply_kernel(x_std)
 
-            if self.collection_mode:
-                self.collection(instance_means=x_mean, instnace_stds=x_std, y_anchors=y_anchor, x_anchors=x_anchor)
-
-            else:
-                assert x.shape[0] == 1 # currently, could support batch size = 1 for kernelized instance normalization
-                top = y_anchor
-                down = y_anchor + 2 * padding + 1
-                left = x_anchor
-                right = x_anchor + 2 *padding + 1
-                x_mean = self.padded_mean_table[:,:,top:down, left:right] # 1, C, H, W
-                x_std = self.padded_std_table[:,:,top:down, left:right] # 1, C, H, W
-                x_mean = self.__multiply_kernel(x_mean)
-                x_std = self.__multiply_kernel(x_std)
-
-            x = (x - x_mean) / x_std * self.weight + self.bias
-            return x
+        x = (x - x_mean) / x_std * self.weight + self.bias
+        return x
 
 def not_use_kernelized_instance_norm(model):
     for _, layer in model.named_modules():
