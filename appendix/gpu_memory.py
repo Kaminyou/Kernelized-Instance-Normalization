0d508c00e852aa33e5751e98d188668ffc11bf04
kaminyou ho <kaminyouho@aetherai.com>
2022-07-04 03:42:38 +0000

    linting

--- appendix/gpu_memory.py
+++ appendix/gpu_memory.py
@@ -8,82 +8,95 @@ import seaborn as sns
 
 if __name__ == "__main__":
     memory_dict = {
-        "training":{
-            "Cyclegan":{
-                "x":[128, 256, 512], "y":[2939, 5517, 16357]
-            },
-            "CUT":{
-                "x":[128, 256, 512], "y":[2365, 4133, 11123]
-            },
-            "F-LSeSim":{
-                "x":[256, 512, 1024], "y":[3585, 8813, 23023]
-            },
-            "L-LSeSim":{
-                "x":[256, 512, 1024], "y":[3605, 9331, 29289]
-            }
+        "training": {
+            "Cyclegan": {"x": [128, 256, 512], "y": [2939, 5517, 16357]},
+            "CUT": {"x": [128, 256, 512], "y": [2365, 4133, 11123]},
+            "F-LSeSim": {"x": [256, 512, 1024], "y": [3585, 8813, 23023]},
+            "L-LSeSim": {"x": [256, 512, 1024], "y": [3605, 9331, 29289]},
         },
-        "inference":{
-            "Cyclegan":{
-                "x":[128, 256, 512, 1024, 2048], "y":[1697, 1795, 2211, 3749, 12073]
+        "inference": {
+            "Cyclegan": {
+                "x": [128, 256, 512, 1024, 2048],
+                "y": [1697, 1795, 2211, 3749, 12073],
             },
-            "CUT":{
-                "x":[128, 256, 512, 1024, 2048], "y":[1602, 1695, 2111, 3654, 11987]
+            "CUT": {
+                "x": [128, 256, 512, 1024, 2048],
+                "y": [1602, 1695, 2111, 3654, 11987],
             },
-            "F-LSeSim":{
-                "x":[512, 1024], "y":[8571, 19269]
+            "F-LSeSim": {"x": [512, 1024], "y": [8571, 19269]},
+            "L-LSeSim": {"x": [512, 1024], "y": [8571, 19269]},
+            "Cyclegan+KIN (ours)": {
+                "x": [128, 256, 512, 1024, 2048, 4096, 9192],
+                "y": [2307, 2307, 2307, 2307, 2307, 2307, 2307],
             },
-            "L-LSeSim":{
-                "x":[512, 1024], "y":[8571, 19269]
+            "CUT+KIN (ours)": {
+                "x": [128, 256, 512, 1024, 2048, 4096, 9192],
+                "y": [2307, 2307, 2307, 2307, 2307, 2307, 2307],
             },
-            "Cyclegan+KIN (ours)":{
-                "x":[128, 256, 512, 1024, 2048, 4096, 9192], "y":[2307, 2307, 2307, 2307, 2307, 2307, 2307]
+            "F/L-LSeSim+KIN (ours)": {
+                "x": [128, 256, 512, 1024, 2048, 4096, 9192],
+                "y": [8581, 8581, 8581, 8581, 8581, 8581, 8581],
             },
-            "CUT+KIN (ours)":{
-                "x":[128, 256, 512, 1024, 2048, 4096, 9192], "y":[2307, 2307, 2307, 2307, 2307, 2307, 2307]
-            },
-            "F/L-LSeSim+KIN (ours)":{
-                "x":[128, 256, 512, 1024, 2048, 4096, 9192], "y":[8581, 8581, 8581, 8581, 8581, 8581, 8581]
-            }
-        }
+        },
     }
 
     pal = sns.color_palette("husl", 8)
     hex_colors = list(map(matplotlib.colors.rgb2hex, pal))
     pal
 
-    marker_list = ['o', 'X','v', 's', '^','p', 'D']
-    plt.figure(figsize=(9,4))
-    for idx, model_name in enumerate(memory_dict['training']):
-        x = memory_dict['training'][model_name]["x"]
-        y = memory_dict['training'][model_name]["y"]
+    marker_list = ["o", "X", "v", "s", "^", "p", "D"]
+    plt.figure(figsize=(9, 4))
+    for idx, model_name in enumerate(memory_dict["training"]):
+        x = memory_dict["training"][model_name]["x"]
+        y = memory_dict["training"][model_name]["y"]
         p2 = np.poly1d(np.polyfit(x, y, 2))
-        
+
         xp = np.linspace(128, x[-1], 100)
         xp_extra = np.linspace(x[-1], 2000, 100)
         marker_on = []
         for x_ in x:
-            marker_on.append(np.searchsorted(xp, x_, side='left'))
+            marker_on.append(np.searchsorted(xp, x_, side="left"))
         # add rnd to avoid overlapping
         rnd = random.randint(0, 200)
-        
-        plt.plot(xp, p2(xp) + rnd, color=hex_colors[idx], linewidth=3.0,linestyle='-',markersize=8,marker=marker_list[idx],markevery=marker_on,label=model_name)
-        plt.plot(xp_extra, p2(xp_extra) + rnd, color=hex_colors[idx], linewidth=3.0,linestyle='--')
-        
-    plt.xticks([128, 256, 512, 1024, 2048, 4096], [128, 256, 512, 1024, 2048, 4096], rotation=45)
+
+        plt.plot(
+            xp,
+            p2(xp) + rnd,
+            color=hex_colors[idx],
+            linewidth=3.0,
+            linestyle="-",
+            markersize=8,
+            marker=marker_list[idx],
+            markevery=marker_on,
+            label=model_name,
+        )
+        plt.plot(
+            xp_extra,
+            p2(xp_extra) + rnd,
+            color=hex_colors[idx],
+            linewidth=3.0,
+            linestyle="--",
+        )
+
+    plt.xticks(
+        [128, 256, 512, 1024, 2048, 4096],
+        [128, 256, 512, 1024, 2048, 4096],
+        rotation=45,
+    )
     plt.yticks([5000, 10000, 15000, 20000, 25000, 30000], [5, 10, 15, 20, 25, 30])
-    plt.ylim(0,32000)
+    plt.ylim(0, 32000)
     plt.title("Training")
     plt.ylabel("GPU Memory (GB)")
     plt.xlabel("Resolution (√x)")
     plt.legend()
     plt.grid()
-    #plt.show()
-    plt.savefig("./training_usage.png", bbox_inches = 'tight')
+    # plt.show()
+    plt.savefig("./training_usage.png", bbox_inches="tight")
 
-    plt.figure(figsize=(9,4))
-    for idx, model_name in enumerate(memory_dict['inference']):
-        x = memory_dict['inference'][model_name]["x"]
-        y = memory_dict['inference'][model_name]["y"]
+    plt.figure(figsize=(9, 4))
+    for idx, model_name in enumerate(memory_dict["inference"]):
+        x = memory_dict["inference"][model_name]["x"]
+        y = memory_dict["inference"][model_name]["y"]
         if not "KIN" in model_name:
             p2 = np.poly1d(np.polyfit(x, y, 2))
         else:
@@ -92,25 +105,51 @@ if __name__ == "__main__":
         xp = np.linspace(128, x[-1], 100)
         xp_extra = np.linspace(x[-1], 10000, 100)
         marker_on = []
-        
+
         # add rnd to avoid overlapping
         rnd = random.randint(0, 2000)
-        
+
         for x_ in x:
-            marker_on.append(np.searchsorted(xp, x_, side='left'))
-        plt.plot(xp, p2(xp) + rnd, color=hex_colors[idx], linewidth=3.0,linestyle='-',markersize=8, marker=marker_list[idx],markevery=marker_on,label=model_name)
+            marker_on.append(np.searchsorted(xp, x_, side="left"))
+        plt.plot(
+            xp,
+            p2(xp) + rnd,
+            color=hex_colors[idx],
+            linewidth=3.0,
+            linestyle="-",
+            markersize=8,
+            marker=marker_list[idx],
+            markevery=marker_on,
+            label=model_name,
+        )
         if "KIN" in model_name:
-            plt.plot(xp_extra, p2(xp_extra) + rnd, color=hex_colors[idx], linewidth=3.0,linestyle='-')
+            plt.plot(
+                xp_extra,
+                p2(xp_extra) + rnd,
+                color=hex_colors[idx],
+                linewidth=3.0,
+                linestyle="-",
+            )
         else:
-            plt.plot(xp_extra, p2(xp_extra) + rnd, color=hex_colors[idx], linewidth=3.0,linestyle='--')
-        
-    plt.xticks([256, 512, 1024, 2048, 4096, 9192], [256, 512, 1024, 2048, 4096, 9192], rotation=45)
+            plt.plot(
+                xp_extra,
+                p2(xp_extra) + rnd,
+                color=hex_colors[idx],
+                linewidth=3.0,
+                linestyle="--",
+            )
+
+    plt.xticks(
+        [256, 512, 1024, 2048, 4096, 9192],
+        [256, 512, 1024, 2048, 4096, 9192],
+        rotation=45,
+    )
     plt.yticks([5000, 10000, 15000, 20000, 25000, 30000], [5, 10, 15, 20, 25, 30])
-    plt.ylim(0,32000)
+    plt.ylim(0, 32000)
     plt.ylabel("GPU Memory (GB)")
     plt.xlabel("Resolution (√x)")
     plt.title("Inference")
     plt.legend()
     plt.grid()
-    #plt.show()
-    plt.savefig("./inference_usage.png", bbox_inches = 'tight')
+    # plt.show()
+    plt.savefig("./inference_usage.png", bbox_inches="tight")
--- appendix/proof_of_concept.py
+++ appendix/proof_of_concept.py
@@ -21,16 +21,18 @@ from utils.util import read_yaml_config, test_transforms
 
 
 def cos_sim(a, b):
-    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 
 def calc_mean_std(feat, eps=1e-5):
     size = feat.shape
-    assert (len(size) == 4)
+    assert len(size) == 4
     N, C = size[:2]
     feat_var = feat.reshape(N, C, -1).var(axis=2) + eps
     feat_std = np.sqrt(feat_var).flatten()
     feat_mean = feat.reshape(N, C, -1).mean(axis=2).flatten()
     return feat_mean, feat_std
+
 
 def compute_img_distance(img_name_a, img_name_b):
     x_a, y_a = img_name_a.split("_")[:2]
@@ -39,25 +41,43 @@ def compute_img_distance(img_name_a, img_name_b):
     y_a = int(y_a)
     x_b = int(x_b)
     y_b = int(y_b)
-    return ((x_a - x_b)**2 + (y_a - y_b)**2)**(1/2)
+    return ((x_a - x_b) ** 2 + (y_a - y_b) ** 2) ** (1 / 2)
+
 
 def main():
     parser = argparse.ArgumentParser("Model inference")
-    parser.add_argument("-c", "--config", type=str, default="./data/example/config.yaml", help="Path to the config file.")
-    parser.add_argument("-o", "--output", type=str, default="./proof_of_concept/", help="Path to the output folder.")
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="./data/example/config.yaml",
+        help="Path to the config file.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="./proof_of_concept/",
+        help="Path to the output folder.",
+    )
     args = parser.parse_args()
 
     os.makedirs(args.output, exist_ok=True)
-    
+
     config = read_yaml_config(args.config)
-    model = get_model(config=config, model_name=config["MODEL_NAME"], normalization=config["INFERENCE_SETTING"]["NORMALIZATION"], isTrain=False)
+    model = get_model(
+        config=config,
+        model_name=config["MODEL_NAME"],
+        normalization=config["INFERENCE_SETTING"]["NORMALIZATION"],
+        isTrain=False,
+    )
 
     test_dataset = XInferenceDataset(
-        root_X=config["INFERENCE_SETTING"]["TEST_DIR_X"], 
-        transform=test_transforms, 
+        root_X=config["INFERENCE_SETTING"]["TEST_DIR_X"],
+        transform=test_transforms,
         return_anchor=True,
-        thumbnail=config["INFERENCE_SETTING"]["THUMBNAIL"]
-        )
+        thumbnail=config["INFERENCE_SETTING"]["THUMBNAIL"],
+    )
 
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, pin_memory=True)
 
@@ -78,158 +98,281 @@ def main():
         X, X_path = data["X_img"], data["X_path"]
         Y_fake, feature_maps = model.analyze_feature_map(X)
         feature_map_dict[Path(X_path[0]).stem] = feature_maps
-    
+
     # precomputed
     print("Precomputing features' mean and var")
     thumbnail_feature_maps_precomputed = {}
     for feature_name, feature_map in feature_map_dict["thumbnail"].items():
-        thumbnail_feature_map_mean, thumbnail_feature_map_std = calc_mean_std(feature_map) # dim = C
-        thumbnail_feature_maps_precomputed[f"{feature_name}_mean"] = thumbnail_feature_map_mean
-        thumbnail_feature_maps_precomputed[f"{feature_name}_std"] = thumbnail_feature_map_std
+        thumbnail_feature_map_mean, thumbnail_feature_map_std = calc_mean_std(
+            feature_map
+        )  # dim = C
+        thumbnail_feature_maps_precomputed[
+            f"{feature_name}_mean"
+        ] = thumbnail_feature_map_mean
+        thumbnail_feature_maps_precomputed[
+            f"{feature_name}_std"
+        ] = thumbnail_feature_map_std
 
     other_featuer_maps_precomputed = defaultdict(dict)
     for img_name, feature_maps in feature_map_dict.items():
-        if img_name == "thumbnail": continue
+        if img_name == "thumbnail":
+            continue
         print(f"Processing ... {img_name}     ", end="\r")
         for feature_name, feature_map in feature_maps.items():
             other_feature_map_mean, other_feature_map_std = calc_mean_std(feature_map)
-            other_featuer_maps_precomputed[img_name][f"{feature_name}_mean"] = other_feature_map_mean
-            other_featuer_maps_precomputed[img_name][f"{feature_name}_std"] = other_feature_map_std
-    
+            other_featuer_maps_precomputed[img_name][
+                f"{feature_name}_mean"
+            ] = other_feature_map_mean
+            other_featuer_maps_precomputed[img_name][
+                f"{feature_name}_std"
+            ] = other_feature_map_std
+
     # thumbnail with others
     print("Calculating thumbnail v.s. patches")
     cos_sim_dict = defaultdict(list)
     for img_name, featuer_maps_precomputed in other_featuer_maps_precomputed.items():
         print(f"Processing ... {img_name}     ", end="\r")
         for feature_name, feature_map_precomputed in featuer_maps_precomputed.items():
-            cos_sim_value = cos_sim(thumbnail_feature_maps_precomputed[feature_name], feature_map_precomputed)
+            cos_sim_value = cos_sim(
+                thumbnail_feature_maps_precomputed[feature_name],
+                feature_map_precomputed,
+            )
             cos_sim_dict[feature_name].append(cos_sim_value)
     print("Drawing ...")
-    for key1, key2 in zip(list(cos_sim_dict.keys())[0::2], list(cos_sim_dict.keys())[1::2]):
+    for key1, key2 in zip(
+        list(cos_sim_dict.keys())[0::2], list(cos_sim_dict.keys())[1::2]
+    ):
         value1 = cos_sim_dict[key1]
         value2 = cos_sim_dict[key2]
 
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,2.5))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 2.5))
         ax1.hist(value1, bins=100)
         ax1.set_title(key1)
         ax1.set_xlabel("Cosine similarity")
         ax1.set_ylabel("# of patches")
-        
+
         ax2.hist(value2, bins=100, range=(-1, 1))
         ax2.set_title(key2)
         ax2.set_xlabel("Cosine similarity")
         ax2.set_ylabel("# of patches")
         if not "resnetblock" in key1:
-            plt.savefig(os.path.join(args.output, f"thumbnail_with_patch_{key1.split('_')[0]}"), bbox_inches = 'tight')
+            plt.savefig(
+                os.path.join(args.output, f"thumbnail_with_patch_{key1.split('_')[0]}"),
+                bbox_inches="tight",
+            )
         else:
-            plt.savefig(os.path.join(args.output, f"thumbnail_with_patch_{key1.rsplit('_', 2)[0]}"), bbox_inches = 'tight')
-        plt.close('all')
+            plt.savefig(
+                os.path.join(
+                    args.output, f"thumbnail_with_patch_{key1.rsplit('_', 2)[0]}"
+                ),
+                bbox_inches="tight",
+            )
+        plt.close("all")
 
     # others with others
     print("Calculating patches v.s. patches")
     cos_sim_dict = defaultdict(list)
-    for i, (img_name_a, featuer_maps_precomputed_a) in enumerate(other_featuer_maps_precomputed.items()):
-        for j, (img_name_b, featuer_maps_precomputed_b) in enumerate(other_featuer_maps_precomputed.items()):
+    for i, (img_name_a, featuer_maps_precomputed_a) in enumerate(
+        other_featuer_maps_precomputed.items()
+    ):
+        for j, (img_name_b, featuer_maps_precomputed_b) in enumerate(
+            other_featuer_maps_precomputed.items()
+        ):
             if i < j:
                 print(f"Processing ... {img_name_a} {img_name_b}     ", end="\r")
                 distance = compute_img_distance(img_name_a, img_name_b)
                 for feature_name in featuer_maps_precomputed_a.keys():
-                    cos_sim_value = cos_sim(featuer_maps_precomputed_a[feature_name], featuer_maps_precomputed_b[feature_name])
+                    cos_sim_value = cos_sim(
+                        featuer_maps_precomputed_a[feature_name],
+                        featuer_maps_precomputed_b[feature_name],
+                    )
                     cos_sim_dict[feature_name].append([distance, cos_sim_value])
-    
+
     print("Drawing ...")
-    for key1, key2 in zip(list(cos_sim_dict.keys())[0::2], list(cos_sim_dict.keys())[1::2]):
+    for key1, key2 in zip(
+        list(cos_sim_dict.keys())[0::2], list(cos_sim_dict.keys())[1::2]
+    ):
         value1 = np.array(cos_sim_dict[key1])
         value2 = np.array(cos_sim_dict[key2])
-        
+
         # filter those are far away
-        value1 = value1[value1[:,0] <= 10]
-        value2 = value2[value2[:,0] <= 10]
+        value1 = value1[value1[:, 0] <= 10]
+        value2 = value2[value2[:, 0] <= 10]
 
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 14))
-        sns.boxplot(ax=ax1, x="distance", y="cos sim", data=pd.DataFrame({"distance":list(map(lambda x: round(x, 2), value1[:,0])), "cos sim":value1[:,1]}))
+        sns.boxplot(
+            ax=ax1,
+            x="distance",
+            y="cos sim",
+            data=pd.DataFrame(
+                {
+                    "distance": list(map(lambda x: round(x, 2), value1[:, 0])),
+                    "cos sim": value1[:, 1],
+                }
+            ),
+        )
         ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45)
-        #ax1.scatter(x=value1[:,0], y=value1[:,1], s=0.2)
+        # ax1.scatter(x=value1[:,0], y=value1[:,1], s=0.2)
         ax1.set_title(key1)
         ax1.set_xlabel("Distance")
         ax1.set_ylabel("Cosine similarity")
-        
-        
-        sns.boxplot(ax=ax2, x="distance", y="cos sim", data=pd.DataFrame({"distance":list(map(lambda x: round(x, 2), value2[:,0])), "cos sim":value2[:,1]}))
-        #ax2.scatter(x=value2[:,0], y=value2[:,1], s=0.2)
+
+        sns.boxplot(
+            ax=ax2,
+            x="distance",
+            y="cos sim",
+            data=pd.DataFrame(
+                {
+                    "distance": list(map(lambda x: round(x, 2), value2[:, 0])),
+                    "cos sim": value2[:, 1],
+                }
+            ),
+        )
+        # ax2.scatter(x=value2[:,0], y=value2[:,1], s=0.2)
         ax2.set_xticklabels(ax2.get_xticklabels(), rotation=45)
         ax2.set_title(key2)
         ax2.set_xlabel("Distance")
         ax2.set_ylabel("Cosine similarity")
         if not "resnetblock" in key1:
-            plt.savefig(os.path.join(args.output, f"patch_with_patch_{key1.split('_')[0]}"), bbox_inches = 'tight')
+            plt.savefig(
+                os.path.join(args.output, f"patch_with_patch_{key1.split('_')[0]}"),
+                bbox_inches="tight",
+            )
         else:
-            plt.savefig(os.path.join(args.output, f"patch_with_patch_{key1.rsplit('_', 2)[0]}"), bbox_inches = 'tight')
-        plt.close('all')
-    
+            plt.savefig(
+                os.path.join(args.output, f"patch_with_patch_{key1.rsplit('_', 2)[0]}"),
+                bbox_inches="tight",
+            )
+        plt.close("all")
+
     # others with others (lineplot)
     colors = ["#4A5FAB", "#609F5C", "#E3C454", "#A27CBA", "#B85031"]
     ## block1 mean
-    plt.figure(figsize=(8,3.2))
+    plt.figure(figsize=(8, 3.2))
     for key1 in list(cos_sim_dict.keys())[0::2]:
         if "block1" in key1:
             value1 = np.array(cos_sim_dict[key1])
-            value1 = value1[value1[:,0] <= 10]
-            df = pd.DataFrame({"Distance (pixel)":value1[:,0] * 512, "Cosine similarity between μ(X)":value1[:,1]})
-            sns.lineplot(data=df, x="Distance (pixel)", y="Cosine similarity between μ(X)", label="layer 1", color=colors[0])
+            value1 = value1[value1[:, 0] <= 10]
+            df = pd.DataFrame(
+                {
+                    "Distance (pixel)": value1[:, 0] * 512,
+                    "Cosine similarity between μ(X)": value1[:, 1],
+                }
+            )
+            sns.lineplot(
+                data=df,
+                x="Distance (pixel)",
+                y="Cosine similarity between μ(X)",
+                label="layer 1",
+                color=colors[0],
+            )
             break
     plt.grid()
-    plt.savefig(os.path.join(args.output, f"patch_with_patch_lineplot_{key1.split('_')[0]}_mean"), bbox_inches='tight')
-    plt.close('all')
+    plt.savefig(
+        os.path.join(
+            args.output, f"patch_with_patch_lineplot_{key1.split('_')[0]}_mean"
+        ),
+        bbox_inches="tight",
+    )
+    plt.close("all")
 
     ## block1 std
-    plt.figure(figsize=(8,3.2))
+    plt.figure(figsize=(8, 3.2))
     for key2 in list(cos_sim_dict.keys())[1::2]:
         if "block1" in key2:
             value2 = np.array(cos_sim_dict[key2])
-            value2 = value2[value2[:,0] <= 10]
-            df = pd.DataFrame({"Distance (pixel)":value2[:,0] * 512, "Cosine similarity between σ(X)":value2[:,1]})
-            sns.lineplot(data=df, x="Distance (pixel)", y="Cosine similarity between σ(X)", label="layer 1", color=colors[0])
+            value2 = value2[value2[:, 0] <= 10]
+            df = pd.DataFrame(
+                {
+                    "Distance (pixel)": value2[:, 0] * 512,
+                    "Cosine similarity between σ(X)": value2[:, 1],
+                }
+            )
+            sns.lineplot(
+                data=df,
+                x="Distance (pixel)",
+                y="Cosine similarity between σ(X)",
+                label="layer 1",
+                color=colors[0],
+            )
             break
     plt.grid()
-    plt.savefig(os.path.join(args.output, f"patch_with_patch_lineplot_{key2.split('_')[0]}_std"), bbox_inches='tight')
-    plt.close('all')
+    plt.savefig(
+        os.path.join(
+            args.output, f"patch_with_patch_lineplot_{key2.split('_')[0]}_std"
+        ),
+        bbox_inches="tight",
+    )
+    plt.close("all")
 
     ## other blocks mean
-    plt.figure(figsize=(10,4))
+    plt.figure(figsize=(10, 4))
     idx = 1
     for key1 in list(cos_sim_dict.keys())[0::2]:
-        if "resnetblock" in key1: continue
-        if "block1" in key1: continue
-        
+        if "resnetblock" in key1:
+            continue
+        if "block1" in key1:
+            continue
+
         value1 = np.array(cos_sim_dict[key1])
 
         # filter those are far away
-        value1 = value1[value1[:,0] <= 10]
-        df = pd.DataFrame({"Distance (pixel)":value1[:,0] * 512, "Cosine similarity between μ(X)":value1[:,1]})
-        sns.lineplot(data=df, x="Distance (pixel)", y="Cosine similarity between μ(X)", label="layer " + key1.split("_")[0][-1], color=colors[idx])
+        value1 = value1[value1[:, 0] <= 10]
+        df = pd.DataFrame(
+            {
+                "Distance (pixel)": value1[:, 0] * 512,
+                "Cosine similarity between μ(X)": value1[:, 1],
+            }
+        )
+        sns.lineplot(
+            data=df,
+            x="Distance (pixel)",
+            y="Cosine similarity between μ(X)",
+            label="layer " + key1.split("_")[0][-1],
+            color=colors[idx],
+        )
         idx += 1
     plt.grid()
-    plt.savefig(os.path.join(args.output, f"patch_with_patch_lineplot_blocks_mean"), bbox_inches='tight')
-    plt.close('all')
+    plt.savefig(
+        os.path.join(args.output, f"patch_with_patch_lineplot_blocks_mean"),
+        bbox_inches="tight",
+    )
+    plt.close("all")
 
     ## other blocks std
-    plt.figure(figsize=(10,4))
+    plt.figure(figsize=(10, 4))
     idx = 1
     for key2 in list(cos_sim_dict.keys())[1::2]:
-        if "resnetblock" in key2: continue
-        if "block1" in key2: continue
+        if "resnetblock" in key2:
+            continue
+        if "block1" in key2:
+            continue
 
         value2 = np.array(cos_sim_dict[key2])
 
         # filter those are far away
-        value2 = value2[value2[:,0] <= 10]
-        df = pd.DataFrame({"Distance (pixel)":value2[:,0] * 512, "Cosine similarity between σ(X)":value2[:,1]})
-        sns.lineplot(data=df, x="Distance (pixel)", y="Cosine similarity between σ(X)", label="layer " + key2.split("_")[0][-1], color=colors[idx])
+        value2 = value2[value2[:, 0] <= 10]
+        df = pd.DataFrame(
+            {
+                "Distance (pixel)": value2[:, 0] * 512,
+                "Cosine similarity between σ(X)": value2[:, 1],
+            }
+        )
+        sns.lineplot(
+            data=df,
+            x="Distance (pixel)",
+            y="Cosine similarity between σ(X)",
+            label="layer " + key2.split("_")[0][-1],
+            color=colors[idx],
+        )
         idx += 1
     plt.grid()
-    plt.savefig(os.path.join(args.output, f"patch_with_patch_lineplot_blocks_std"), bbox_inches='tight')
-    plt.close('all')
+    plt.savefig(
+        os.path.join(args.output, f"patch_with_patch_lineplot_blocks_std"),
+        bbox_inches="tight",
+    )
+    plt.close("all")
+
 
 if __name__ == "__main__":
     main()
