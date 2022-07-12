1259d523940c71753cc5a5c11f8c2ba56366ee5f
vincentwu1 <vincentwu@aetherai.com>
2022-07-12 06:38:51 +0000

    Complete integration testing for CUT

--- models/tests/test_cut.py
+++ models/tests/test_cut.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 import numpy as np
 import pytest
@@ -7,32 +6,49 @@ import torch
 from models.model import get_model
 from PIL import Image
 from torch.utils.data import DataLoader
-from torchvision.utils import make_grid, save_image
+from torchvision.utils import make_grid
 from utils.dataset import XInferenceDataset
 from utils.util import (read_yaml_config, reverse_image_normalize,
                         test_transforms)
 
-from ..cut import ContrastiveModel
-
 
 @pytest.fixture()
 def config():
-    config_path = '/home/vincentwu/URUST_exp/config_lung_lesion_for_paper.yaml'
+    config_path = os.path.join(
+        '/home/vincentwu',
+        'URUST_exp',
+        'config_lung_lesion_for_test_cut.yaml'
+    )
     config = read_yaml_config(config_path)
 
     return config
 
+
 @pytest.fixture()
-def model(config):
+def in_model(config):
     model = get_model(
         config=config,
         model_name=config["MODEL_NAME"],
-        normalization=config["INFERENCE_SETTING"]["NORMALIZATION"],
+        normalization='in',
         isTrain=False,
     )
     model.load_networks(config["INFERENCE_SETTING"]["MODEL_VERSION"])
 
     return model
+
+
+@pytest.fixture()
+def kin_model(config):
+    model = get_model(
+        config=config,
+        model_name=config["MODEL_NAME"],
+        normalization='kin',
+        isTrain=False,
+    )
+    model.load_networks(config["INFERENCE_SETTING"]["MODEL_VERSION"])
+
+    return model
+
 
 @pytest.fixture()
 def dataset(config):
@@ -43,6 +59,7 @@ def dataset(config):
     )
     return dataset
 
+
 @pytest.fixture()
 def dataloader(dataset):
     loader = DataLoader(
@@ -50,30 +67,93 @@ def dataloader(dataset):
     )
     return loader
 
+
 @pytest.fixture()
-def save_path_base(config):
-    save_path_root = os.path.join(
+def in_expected_outputs(config):
+    expected_output_dir = os.path.join(
         config["EXPERIMENT_ROOT_PATH"],
         config["EXPERIMENT_NAME"],
-        "test"
+        "test",
+        "in",
+        f"{config['INFERENCE_SETTING']['MODEL_VERSION']}_pytest",
+
     )
-    save_path_base = os.path.join(
-        save_path_root,
-        config["INFERENCE_SETTING"]["NORMALIZATION"],
+    expected_output_files = sorted(
+        os.listdir(expected_output_dir)
+    )
+
+    expected_outputs = []
+    for expected_output_file in expected_output_files:
+        expected_outputs.append(np.array(
+            Image.open(os.path.join(
+                expected_output_dir,
+                expected_output_file
+            )).convert("RGB")
+        ))
+
+    return expected_outputs
+
+
+@pytest.fixture()
+def kin_expected_outputs(config):
+    expected_output_dir = os.path.join(
+        config["EXPERIMENT_ROOT_PATH"],
+        config["EXPERIMENT_NAME"],
+        "test",
+        "kin",
         config["INFERENCE_SETTING"]["MODEL_VERSION"],
-    )
-    save_path_base_kin = os.path.join(
-        save_path_base,
         f"{config['INFERENCE_SETTING']['KIN_KERNEL']}_"
-        f"{config['INFERENCE_SETTING']['KIN_PADDING']}",
+        f"{config['INFERENCE_SETTING']['KIN_PADDING']}_pytest",
+
+    )
+    expected_output_files = sorted(
+        os.listdir(expected_output_dir)
     )
 
-    return save_path_base_kin
+    expected_outputs = []
+    for expected_output_file in expected_output_files:
+        expected_outputs.append(np.array(
+            Image.open(os.path.join(
+                expected_output_dir,
+                expected_output_file
+            )).convert("RGB")
+        ))
 
-def test_inference_with_anchor(config, model, dataset, dataloader, save_path_base):
+    return expected_outputs
+
+
+def test_inference(in_model, dataloader, in_expected_outputs):
+    """
+        Integration testing for IN inferece
+    """
+
+    for idx, data in enumerate(dataloader):
+        X, _ = data["X_img"], data["X_path"]
+        Y_fake = in_model.inference(X)
+        test_output_tensor = reverse_image_normalize(Y_fake)
+
+        # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
+        test_output = make_grid(test_output_tensor).mul(255).add_(0.5).clamp_(
+            0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+        expected_output = in_expected_outputs[idx]
+
+        assert test_output == pytest.approx(expected_output)
+
+
+def test_inference_with_anchor(
+    config,
+    kin_model,
+    dataset,
+    dataloader,
+    kin_expected_outputs
+):
+    """
+        Integration testing for KIN inferece
+    """
+
     y_anchor_num, x_anchor_num = dataset.get_boundary()
 
-    model.init_kernelized_instance_norm_for_whole_model(
+    kin_model.init_kernelized_instance_norm_for_whole_model(
         y_anchor_num=y_anchor_num + 1,
         x_anchor_num=x_anchor_num + 1,
         kernel_padding=config["INFERENCE_SETTING"]["KIN_PADDING"],
@@ -81,31 +161,31 @@ def test_inference_with_anchor(config, model, dataset, dataloader, save_path_bas
     )
 
     for idx, data in enumerate(dataloader):
-        X, X_path, y_anchor, x_anchor = (
+        X, _, y_anchor, x_anchor = (
             data["X_img"],
             data["X_path"],
             data["y_idx"],
             data["x_idx"],
         )
-        _ = model.inference_with_anchor(
+        _ = kin_model.inference_with_anchor(
             X,
             y_anchor=y_anchor,
             x_anchor=x_anchor,
             padding=config["INFERENCE_SETTING"]["KIN_PADDING"],
         )
 
-    model.use_kernelized_instance_norm_for_whole_model(
+    kin_model.use_kernelized_instance_norm_for_whole_model(
         padding=config["INFERENCE_SETTING"]["KIN_PADDING"]
     )
 
     for idx, data in enumerate(dataloader):
-        X, X_path, y_anchor, x_anchor = (
+        X, _, y_anchor, x_anchor = (
             data["X_img"],
             data["X_path"],
             data["y_idx"],
             data["x_idx"],
         )
-        Y_fake = model.inference_with_anchor(
+        Y_fake = kin_model.inference_with_anchor(
             X,
             y_anchor=y_anchor,
             x_anchor=x_anchor,
@@ -113,42 +193,9 @@ def test_inference_with_anchor(config, model, dataset, dataloader, save_path_bas
         )
         test_output_tensor = reverse_image_normalize(Y_fake)
 
-        save_path_root = os.path.join(
-            config["EXPERIMENT_ROOT_PATH"],
-            config["EXPERIMENT_NAME"],
-            "test"
-        )
-        save_dir = os.path.join(
-            save_path_root,
-            config["INFERENCE_SETTING"]["NORMALIZATION"],
-            config["INFERENCE_SETTING"]["MODEL_VERSION"],
-            f"{config['INFERENCE_SETTING']['KIN_KERNEL']}_"
-            f"{config['INFERENCE_SETTING']['KIN_PADDING']}_pytest",
-        )
-        
-        os.makedirs(save_dir, exist_ok=True)
+        # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
+        test_output = make_grid(test_output_tensor).mul(255).add_(0.5).clamp_(
+            0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+        expected_output = kin_expected_outputs[idx]
 
-        test_output_path = os.path.join(
-            save_dir,
-            f"{Path(X_path[0]).stem}_Y_fake_{idx}.png",
-        )
-        save_image(
-            test_output_tensor,
-            test_output_path
-        )
-        test_output = np.array(
-            Image.open(test_output_path).convert("RGB")
-        )
-        # # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
-        # test_output = make_grid(test_output_tensor).mul(255).add_(0.5).clamp_(
-        #     0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
-
-        expected_output_path = os.path.join(
-            save_path_base,
-            f"{Path(X_path[0]).stem}_Y_fake_{idx}.png",
-        )
-        expected_output = np.array(
-            Image.open(expected_output_path).convert("RGB")
-        )
-        print(test_output == pytest.approx(expected_output, abs=1e-6))
-        assert test_output == pytest.approx(expected_output, abs=1e-6)
+        assert test_output == pytest.approx(expected_output)
